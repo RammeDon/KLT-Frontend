c36e6320f1445dde04518632a79976cf6d33971b
sam-hur <83226062+sam-hur@users.noreply.github.com>
2022-12-11 19:07:50 +0100

    [refactor] Moved password strength-checker fun to ResetPasswordScreen
    ... Users who log in will not need to be reminded of their password strength, as this is only relevant during password reset

--- KLT/app/src/main/java/com/klt/screens/LoginScreen.kt
+++ KLT/app/src/main/java/com/klt/screens/LoginScreen.kt
@@ -11,41 +11,20 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.klt.R
 import com.klt.ui.navigation.Home
 
 val backgroundColor = Color.White
-
-private fun strengthChecker(password: String): StrengthPasswordTypes =
-    when {
-        REGEX_STRONG_PASSWORD.toRegex().containsMatchIn(password) -> StrengthPasswordTypes.STRONG
-        else -> StrengthPasswordTypes.WEAK
-    }
-
-enum class StrengthPasswordTypes {
-    STRONG,
-    WEAK
-}
-
-private const val REGEX_STRONG_PASSWORD =
-    "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~\$^+=<>]).{8,20}\$"
 
 
 @Composable
@@ -87,7 +66,6 @@ fun LoginScreen(
 
             PasswordTextField(
                 text = pw,
-                validateStrengthPassword = true,
                 hasError = false,
                 onTextChanged = { newVal: String -> pw = newVal }
             )
@@ -115,20 +93,14 @@ fun LoginScreen(
 
         }
     }
-
-
 }
 
-/*  code inspired by Juan Guillermo Gómez Torres
- Source: https://medium.com/google-developer-experts/how-to-create-a-composable-password-with-jetpack-compose-f1be2d48d9f0
- */
+
 @Composable
 fun PasswordTextField(
     text: String,
     semanticContentDescription: String = "",
-    validateStrengthPassword: Boolean = false,
     hasError: Boolean = false,
-    onHasStrongPassword: (isStrong: Boolean) -> Unit = {},
     onTextChanged: (text: String) -> Unit,
 ) {
     val focusManager = LocalFocusManager.current
@@ -156,57 +128,4 @@ fun PasswordTextField(
         isError = hasError,
         visualTransformation = if (showPassword.value) VisualTransformation.None else PasswordVisualTransformation(),
     )
-    Spacer(modifier = Modifier.height(8.dp))
-    if (validateStrengthPassword) {
-        val strengthPasswordType = strengthChecker(text)
-        if (strengthPasswordType == StrengthPasswordTypes.STRONG) {
-            onHasStrongPassword(true)
-        } else {
-            onHasStrongPassword(false)
-        }
-        Text(
-            modifier = Modifier
-                .padding(start = 12.dp, top = 10.dp)
-                .semantics {
-                    contentDescription = "StrengthPasswordMessage"
-                }
-                .alpha(if (text == "") 0f else 100f),
-            text = buildAnnotatedString {
-                withStyle(
-                    style = SpanStyle(
-                        color = Color.DarkGray,
-                        fontSize = 10.sp,
-                    )
-                ) {
-                    append(stringResource(id = R.string.warning_password_level))
-                    withStyle(
-                        style = SpanStyle(
-                            color = if (strengthPasswordType == StrengthPasswordTypes.STRONG)
-                                Color(0xFF52c202)
-                            else Color(0xFFD10000)
-                        )
-                    ) {
-                        when (strengthPasswordType) {
-                            StrengthPasswordTypes.STRONG ->
-                                append(
-                                    " ${
-                                        stringResource(id = R.string.warning_password_level_strong)
-                                    }"
-                                )
-                            StrengthPasswordTypes.WEAK ->
-                                append(
-                                    " ${
-                                        stringResource(id = R.string.warning_password_level_weak)
-                                    }"
-                                )
-
-
-                        }
-                    }
-                }
-            }
-        )
-    }
-
-
 }
--- KLT/app/src/main/java/com/klt/screens/ResetPasswordScreen.kt
+++ KLT/app/src/main/java/com/klt/screens/ResetPasswordScreen.kt
@@ -1,8 +1,38 @@
 package com.klt.screens
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.klt.R
+
+private fun strengthChecker(password: String): StrengthPasswordTypes =
+    when {
+        REGEX_STRONG_PASSWORD.toRegex().containsMatchIn(password) -> StrengthPasswordTypes.STRONG
+        else -> StrengthPasswordTypes.WEAK
+    }
+
+enum class StrengthPasswordTypes {
+    STRONG,
+    WEAK
+}
+
+private const val REGEX_STRONG_PASSWORD =
+    "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~\$^+=<>]).{8,20}\$"
+
 
 @Composable
 fun ResetPasswordScreen(
@@ -11,4 +41,63 @@ fun ResetPasswordScreen(
     OnSelfClick: () -> Unit = {}
 ) {
     /*TODO */
+
+    
+}
+
+@Composable
+private fun CheckPasswordStrength(
+    validateStrengthPassword: Boolean = false,
+    onHasStrongPassword: (isStrong: Boolean) -> Unit = {},
+    text: String
+) {
+    Spacer(modifier = Modifier.height(8.dp))
+    if (validateStrengthPassword) {
+        val strengthPasswordType = strengthChecker(text)
+        if (strengthPasswordType == StrengthPasswordTypes.STRONG) {
+            onHasStrongPassword(true)
+        } else {
+            onHasStrongPassword(false)
+        }
+        Text(
+            modifier = Modifier
+                .padding(start = 12.dp, top = 10.dp)
+                .semantics {
+                    contentDescription = "StrengthPasswordMessage"
+                }
+                .alpha(if (text == "") 0f else 100f),
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.DarkGray,
+                        fontSize = 10.sp,
+                    )
+                ) {
+                    append(stringResource(id = R.string.warning_password_level))
+                    withStyle(
+                        style = SpanStyle(
+                            color = if (strengthPasswordType == StrengthPasswordTypes.STRONG)
+                                Color(0xFF52c202)
+                            else Color(0xFFD10000)
+                        )
+                    ) {
+                        when (strengthPasswordType) {
+                            StrengthPasswordTypes.STRONG ->
+                                append(
+                                    " ${
+                                        stringResource(id = R.string.warning_password_level_strong)
+                                    }"
+                                )
+                            StrengthPasswordTypes.WEAK ->
+                                append(
+                                    " ${
+                                        stringResource(id = R.string.warning_password_level_weak)
+                                    }"
+                                )
+                        }
+                    }
+                }
+            }
+        )
+    }
 }
