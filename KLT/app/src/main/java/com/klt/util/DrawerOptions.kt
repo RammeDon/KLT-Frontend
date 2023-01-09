41c308711ead9bd5586d9f888f29065163240971
sam-hur <83226062+sam-hur@users.noreply.github.com>
2023-01-09 15:53:18 +0100

    [refactor] Removed dropped Export feature

--- KLT/app/src/main/java/com/klt/util/DrawerOptions.kt
+++ KLT/app/src/main/java/com/klt/util/DrawerOptions.kt
@@ -8,7 +8,6 @@ import com.klt.ui.navigation.*
 enum class SideBarUserOptions(val title: String, val icon: ImageVector?, val route: String) {
     PROFILE("Profile", User.icon, User.route),
     SETTINGS("Settings", Settings.icon, Settings.route),
-    EXPORT("Export", null, Login.route), /* TODO -- ROUTE STILL NEEDED */
     LOGOUT("Logout", null, Logout.route)
 }
 
@@ -16,11 +15,7 @@ enum class SideBarUserOptions(val title: String, val icon: ImageVector?, val rou
 enum class SideBarAdminOptions(val title: String, val icon: ImageVector?, val route: String) {
     CREATE_USER("Create User", CreateUser.icon, route = CreateUser.route),
     USER_CONTROL("User Control", UserControl.icon, route = UserControl.route),
-
-    //   Needs to list all customers + edit and delete options
     CUSTOMER_CONTROL("Customer control", CustomerControl.icon, route = CustomerControl.route),
-
-    //    TASK_TEMPLATES("Task templates", Tasks.icon, route = Tasks.route),  # dropped
 
     // Needs to be routed to Alrik's screen
     PERFORMANCE("Performance", Icons.Rounded.Analytics, route = Login.route)
